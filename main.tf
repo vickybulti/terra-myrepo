84def4faf0faac8bfac6a8e3c574f070b34f2d06
vishal saini <vickybulti@VickyBultiMac.local>
2025-12-12 00:57:25 +0530

    c2

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "res1" {
   ami           = "ami-03978d951b279ec0b"
-  instance_type = t2.micro
+  instance_type = "t2.micro"
   
  tags = {
- Name = inst-vcs1
+ Name = "my-vcs-inst1"
 }
 
 }
